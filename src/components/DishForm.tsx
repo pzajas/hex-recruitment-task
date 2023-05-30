66c6356b217b1f0214b671b0e02ebfbf9037a20d
pzajas <zajas.piotr@gmail.com>
2023-05-30 13:19:10 +0200

    Apply initial styles using styled-components.

--- src/components/DishForm.tsx
+++ src/components/DishForm.tsx
@@ -3,6 +3,7 @@ import { yupResolver } from '@hookform/resolvers/yup'
 import { dishConfig } from '../variables/arrays'
 import { DishConfig, DishField, CustomFieldValues } from '../typescript/types'
 import { validationSchema } from '../validation/schema'
+import styled from 'styled-components'
 
 import axios from 'axios'
 
@@ -47,49 +48,118 @@ const DishForm = () => {
   )
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label>Name:</label>
-        <input {...register('name')} />
+    <StyledFormWrapper onSubmit={handleSubmit(onSubmit)}>
+      <StyledInputWrapper>
+        <StyledLabel>Name</StyledLabel>
+        <StyledInput
+          {...register('name')}
+          placeholder="Name of the dish"
+          autoComplete="off"
+        />
         {errors.name && <span>{errors.name.message}</span>}
-      </div>
+      </StyledInputWrapper>
 
-      <div>
-        <label>Preparation Time:</label>
-        <input {...register('preparation_time')} placeholder="HH:MM:SS" />
+      <StyledInputWrapper>
+        <StyledLabel>Preparation Time</StyledLabel>
+        <StyledInput
+          {...register('preparation_time')}
+          placeholder="HH:MM:SS"
+          autoComplete="off"
+        />
         {errors.preparation_time && (
           <span>{errors.preparation_time.message}</span>
         )}
-      </div>
+      </StyledInputWrapper>
 
-      <div>
-        <label>Type:</label>
-        <select {...register('type')}>
+      <StyledInputWrapper>
+        <StyledLabel>Type</StyledLabel>
+        <StyledSelect {...register('type')} autoComplete="off">
           <option value="">Select Type</option>
           <option value="pizza">Pizza</option>
           <option value="soup">Soup</option>
           <option value="sandwich">Sandwich</option>
-        </select>
+        </StyledSelect>
         {errors.type && <span>{errors.type.message}</span>}
-      </div>
+      </StyledInputWrapper>
 
       {currentDishConfig &&
         currentDishConfig.fields.map((field: DishField) => (
-          <div key={field.name}>
-            <label>{field.label}:</label>
-            <input
+          <StyledSelectWrapper key={field.name}>
+            <StyledLabel>{field.label}:</StyledLabel>
+            <StyledInput
               {...register(field.name, field.validation)}
               type={field.type}
               step={field.step}
               placeholder={field.placeholder}
             />
             {errors[field.name] && <span>{errors[field.name].message}</span>}
-          </div>
+          </StyledSelectWrapper>
         ))}
 
-      <button type="submit">Submit</button>
-    </form>
+      <StyledButton type="submit">Submit</StyledButton>
+    </StyledFormWrapper>
   )
 }
 
 export default DishForm
+
+const StyledFormWrapper = styled.form`
+  display: flex;
+  flex-direction: column;
+  padding: 2rem;
+`
+
+const StyledInputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const StyledSelectWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+const StyledInput = styled.input`
+  padding: 0.2rem 0.5rem;
+  margin-bottom: 1rem;
+  font-size: 1rem;
+
+  background-color: #252525;
+
+  border: none;
+  outline: none;
+`
+
+const StyledSelect = styled.select`
+  font-size: 1rem;
+  padding: 0.2rem 0.5rem;
+  margin-bottom: 1rem;
+  width: 33%;
+
+  background-color: #252525;
+
+  display: flex;
+  align-items: center;
+
+  border: none;
+  outline: none;
+`
+
+const StyledLabel = styled.label`
+  font-size: 0.6rem;
+  margin-bottom: 0.5rem;
+  display: flex;
+  align-items: center;
+`
+
+const StyledButton = styled.button`
+  margin-top: 2rem;
+  border-radius: 0.1rem;
+  font-size: 0.9rem;
+  font-weight: 600;
+  width: 25%;
+  padding: 0.2rem 0.5rem;
+  border: none;
+  background-color: lightgreen;
+  color: #181818;
+`
